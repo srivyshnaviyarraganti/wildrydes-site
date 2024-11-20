d23fc924d87d3aeb9b9b2f42ad42109da8c5536c
sxy220038 <vyshnavi.yarraganti70@gmail.com>
2024-11-20 16:04:12 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-1_yiXZ9hULQ
-        userPoolClientId: '', // e.g. 2csp2r0akpspk0ef0kkt65kgn1
-        region: '' // e.g. us-east-1
+        userPoolId: 'us-east-1_yiXZ9hULQ',
+        userPoolClientId: '2csp2r0akpspk0ef0kkt65kgn1',
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // Leave this blank for now
     }
 };
