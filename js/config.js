5330b46f33a3b88e2993db71bf0dc69941e405c3
sxy220038 <vyshnavi.yarraganti70@gmail.com>
2024-11-20 19:33:47 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_yiXZ9hULQ',
-        userPoolClientId: '2csp2r0akpspk0ef0kkt65kgn1',
+        userPoolId: 'us-east-1_r6FF6uNlA',
+        userPoolClientId: '778thj40njdievjejsqanm0k78',
         region: 'us-east-1'
     },
     api: {
-        invokeUrl: 'https://817tbqm3je.execute-api.us-east-1.amazonaws.com/dev'
+        invokeUrl: // 'https://817tbqm3je.execute-api.us-east-1.amazonaws.com/dev'
     }
 };
